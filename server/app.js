afc2bb41d316bff410d48abc3721ffa8a9a2dab5
aamir-coding <aamirmkh25@gmail.com>
2026-05-11 23:23:21 +0530

    Fix trust proxy for Render + correct Gemini 2.5 flash model ID

--- server/app.js
+++ server/app.js
@@ -8,6 +8,7 @@ const connectDB = require('./config/db');
 require('dotenv').config();
 
 const app = express();
+app.set('trust proxy', 1);
 const server = http.createServer(app);
 
 const io = new Server(server, {
